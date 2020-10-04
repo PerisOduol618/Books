4db7435d12d0249149831a6852b58fc7b9d6260a
Oduol Peris <oduolpepe618@gmail.com>
2020-10-04 20:51:57 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
- export BOOKS_API_KEY=AIzaSyBR0tWeAXgFn6DaDFEYfbjYsUzFcRvONRI
- export SECRET_KEY=fn0AyPYzJhHBZBafcFbfyA
-
- export MAIL_USERNAME='oduolpepe618@gmail.com'
- export MAIL_PASSWORD='37959713Sam'
-
- python3.6 manage.py server
